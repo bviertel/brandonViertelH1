af347f11388ec29130c1f6dc07043967ac67903b
Brandon <bviertel@gmail.com>
2016-09-07 16:39:24 -0400

    Added class "object"
    With getName, and getWeight

---
+++ rpg.java
@@ -0,0 +1,20 @@
+public class object{
+  private String name;
+  private int weight;
+  
+  public String getName() {
+    return name;
+  }
+  
+  public int getWeight() {
+    return weight;
+  }
+  
+  public 
+
+
+public class rpg {
+  public static void main(String[] args) {
+  }
+}
+
